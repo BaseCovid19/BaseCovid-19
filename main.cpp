f80a6d7f89490027bda095b273f0691d062fc6f2
sebaspb234 <62064772+sebaspb234@users.noreply.github.com>
2020-06-22 19:23:50 -0500

    BaseCovid19

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void imprimirLista()
 }
 
 void menu();
-
+void return_menu();
 int main()
 {
     menu();
@@ -41,33 +41,72 @@ void menu(){
             {
                 cout<<"1.Mostrar pacientes registrados\n";
                 cout<<"2.Resgistrar paciente\n";
+                cout<<"3.Actualizar estado de los pacientes\n";
                 cin>>eleccion;
                 cin.ignore();
+                system("cls");
                 if(eleccion==1)
                 {
-                    vulnerable.mostrar_Registro();
+                    cout<<"1.Pacientes vulnerables\n";
+                    cout<<"2.Pacientes no vulnerables\n";
+                    cin>>eleccion;
+                    cin.ignore();
+                    system("cls");
+                    if(eleccion==1){
+                        vulnerable.mostrar_Registro();
+                        return_menu();
+                    }
+                    else if(eleccion==2){
+                        noVulnerable.mostrar_Registro();
+                        return_menu();
+                    }
                 }
-                if(eleccion==2){
+                else if(eleccion==2){
                     cout<<"El paciente tiene mas de 60 anios?   SI/NO \n";
                     getline(cin,verificacion);
                     system("cls");
                     if(verificacion=="SI"||verificacion=="Si"||verificacion=="si")
                     {
-                            vulnerable.Registro(); system("cls");
-                            vulnerable.mostrarDatos();
-                            vulnerable.registro_Paciente();
-                            vulnerable.centroRecomendado();
-                            menu();
+                            vulnerable.Registro();
+                            if(vulnerable.getEdad()<60){
+                                    cout<<"Edad incorrecta, vuelva a ingresar\n";
+                                return_menu();
+                            }
+                            else{
+                                vulnerable.registro_Paciente();
+                                vulnerable.centroRecomendado();
+                                return_menu();}
+
                     }
                     else if(verificacion=="NO"||verificacion=="No"||verificacion=="no")
                     {
-                        noVulnerable.Registro();
-                        noVulnerable.mostrarDatos();
+                            noVulnerable.Registro();
+                            if(noVulnerable.getEdad()>60){
+                                cout<<"Edad incorrecta, vuelva a ingresar\n";
+                                return_menu();
+                            }
+                            else{
+                                noVulnerable.registro_Paciente();
+                                return_menu();
+                            }
                     }
+                }
+                else if(eleccion==3) // Actualizar el estado del paciente
+                {
+                    vulnerable.estado_Paciente();
+                    return_menu();
                 }
             }
             else if(eleccion==2){paciente.Sintomas();}
             else if(eleccion==3){paciente.AntecedentesMedicos();}
-            else if(eleccion==5){return;}
+            else if(eleccion==5){return ;}
         }while(eleccion!=1||eleccion!=2||eleccion!=3||eleccion!=4);
     }
+
+void return_menu()
+{
+    string volver;
+    cout<<"\nPresiona v para volver al menu\n";getline(cin,volver);
+    system("cls");
+    if(volver=="v"){menu();}
+}
