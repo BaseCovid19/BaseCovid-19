d538498c6a3a2cd9b23ca03657552a5495445b4c
sebaspb234 <62064772+sebaspb234@users.noreply.github.com>
2020-07-02 08:50:51 -0500

    BaseCovid19

--- include/Poblacion.h
+++ include/Poblacion.h
@@ -1,9 +1,11 @@
 #ifndef POBLACION_H
 #define POBLACION_H
+#include <vector>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 
@@ -19,6 +21,13 @@ class Poblacion
        string estado;
        bool encontrado;
        string nombre_aux,estado_aux;
+       vector<string> vec;
+       int contadorPacientes;
+       int contadorFallecidos;
+       int contadorRecuperados;
+       bool alta;
+       bool muerto;
+       bool recuperado;
 
    public:
 
@@ -30,6 +39,8 @@ class Poblacion
        void setSintomas(string sintoma);
        void setAntecedentes(string antecedente);
        void setEstado(string _estado);
+       void setContador(int cont);
+       void setContf(int contf);
        string getNombre();
        string getApellido();
        int getEdad();
@@ -37,6 +48,8 @@ class Poblacion
        string getSintomas();
        string getAntecedente();
        string getEstado();
+       int getContador();
+       int getContf();
        string Sintomas();
        string AntecedentesMedicos();
        virtual string Registro();
@@ -45,7 +58,18 @@ class Poblacion
        virtual void mostrar_Registro();
        virtual void estado_Paciente();// modifica el estado del paciente
        virtual void alta_Paciente();
+       virtual void muerte_Paciente();
 
+       void vector_nombres();
+       void aumentar_contador();
+       void crear_contador(); // Cuando pase algun error y se borre el archivo contador
+       void reducir_contador();
+
+       void crear_contf();
+       void aumentar_contf();
+
+       void crear_contr();
+       void aumentar_contr();
 
 
 
